c76a74e5435c64afeb293e245eff4bac79afcb9b
Harrison <hmseaborn@gmail.com>
2021-07-03 13:52:31 -0700

    wip

---
+++ redux-practice/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">\
+    <section className="list">
+      <h1> list </h1>
+      <ul>
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+        </ul>
+        </section>
+    </div>
+  );
+}
+
+export default App;
