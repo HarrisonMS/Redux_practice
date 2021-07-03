e6564c9e627d1cd2676de2359d7efd1da6641a82
Harrison <hmseaborn@gmail.com>
2021-07-03 14:14:28 -0700

    wip

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,20 +1,40 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">\
-    <section className="list">
-      <h1> list </h1>
-      <ul>
-        <li>Hi</li>
-        <li>Hi</li>
-        <li>Hi</li>
-        <li>Hi</li>
-        </ul>
-        </section>
-    </div>
-  );
+	return (
+		<div className="container">
+			<section className="list">
+				<h1> list </h1>
+				<ul>
+					<li>Hi</li>
+					<li>Hi</li>
+					<li>Hi</li>
+					<li>Hi</li>
+				</ul>
+			</section>
+			<section className="circles">
+				<h1> list </h1>
+				<span class="dot"></span>
+				<span class="dot"></span>
+				<span class="dot"></span>
+				<span class="dot"></span>
+			</section>
+			<section className="circles">
+				<h1> list </h1>
+				<span class="dot"></span>
+				<span class="dot"></span>
+				<span class="dot"></span>
+				<span class="dot"></span>
+			</section>
+			<section className="circles">
+				<h1> list </h1>
+				<span class="dot"></span>
+				<span class="dot"></span>
+				<span class="dot"></span>
+				<span class="dot"></span>
+			</section>
+		</div>
+	);
 }
 
 export default App;
